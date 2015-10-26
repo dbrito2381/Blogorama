2ad6e0510b36bac76575b2cc84902e05b278f6d5
Daniel Brito <dbrito2381@hotmail.com>
2015-10-25 22:04:26 -0400

    Blog working but no forms show up and no authentication implemented yet

---
+++ db/migrate/20151025234647_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.string :string
+      t.string :body
+      t.string :text
+      t.references :post, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
